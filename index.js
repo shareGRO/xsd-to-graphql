ae87b0053978177d2cb311cdf9443f8088cdc310
Brandon Brotsky <Brandon@brotskydesigns.com>
2020-04-03 19:19:12 -0700

    updates to graphql json (almost working)

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 const Xsd2JsonSchema = require('xsd2jsonschema').Xsd2JsonSchema;
+const { isNil, get, uniq, split, last, toUpper } = require('lodash');
+
+const { graphQLTypes } = require('./graphql/types');
+// const { introspectionQuery } = require('graphql'); 
  
 const getAllFiles = function(dirPath, arrayOfFiles) {
   files = fs.readdirSync(dirPath)
@@ -18,9 +22,75 @@ const getAllFiles = function(dirPath, arrayOfFiles) {
   return arrayOfFiles
 }
 
+const isAttr = (prop) => String(prop).substr(0,1) === '@';
+
+const getFields = (properties) => {
+  if (isNil(properties) ) return null;
+  // console.log('properties', properties);
+  const propKeys = Object.keys(properties);
+  const fields = propKeys.map((key) => {
+    const isAttribute = isAttr(key);
+    const description = get(properties[key], 'description', null);
+    const ref = last(split(get(properties[key], '$ref', null), '/'));
+    const name = isAttribute ? key.substr(1, key.length) : key;
+
+    const typeName = ref !== '' ? ref : get(properties[key], 'type', name);
+
+    const type = isAttribute ? {
+      kind: 'OBJECT',
+      name: typeName,
+     } : {
+      kind: 'OBJECT',
+      name: typeName,
+     };
+    
+    return {
+      name,
+      isAttribute,
+      description,
+      type,
+      args: [],
+      isDeprecated: false,
+      deprecationReason: null,
+      // debug: properties,
+    }
+  })
+  return fields;
+}
+
+const getScalars = (data) => {
+  const keys = Object.keys(data);
+  const scalars = [];
+  keys.forEach((key) => {
+    const {
+      properties,
+    } = dataObjects[key];
+    if (!isNil(properties)) {
+      const propKeys = Object.keys(properties);
+      propKeys.forEach((propKey) => {
+        const prop = properties[propKey];
+        const { type } = prop;
+        if(!isNil(prop.type)) scalars.push(toUpper(type));
+      });
+    }
+    return properties;
+  });
+  const uniqScalars = uniq(scalars)
+  return uniqScalars.map((s) => ({
+    kind: 'SCALAR',
+    name: s,
+    description: `It's a ${s}`,
+    fields: null,
+    inputFields: null,
+    interfaces: null,
+    enumValues: null,
+    possibleTypes: null
+  }));
+};
+
 const allFiles = getAllFiles('xsd');
 
-let allDefinitions = {};
+let dataObjects = {};
 
 allFiles.forEach((file) => {
   var XML_SCHEMA = fs.readFileSync(file, 'utf8');
@@ -28,23 +98,60 @@ allFiles.forEach((file) => {
   const xs2js = new Xsd2JsonSchema();
    
   const convertedSchemas = xs2js.processAllSchemas({
-      schemas: {'hello_world.xsd': XML_SCHEMA}
+      schemas: {'xsd': XML_SCHEMA}
   });
-  const jsonSchema = convertedSchemas['hello_world.xsd'].getJsonSchema();
+  const jsonSchema = convertedSchemas['xsd'].getJsonSchema();
 
   const { definitions } = jsonSchema;
   
-  allDefinitions = {
-    ...allDefinitions,
+  dataObjects = {
+    ...dataObjects,
     ...definitions,
   };
 })
 
-// console.log('allDefinitions', allDefinitions);
+const scalars = getScalars(dataObjects);
+const keys = Object.keys(dataObjects);
+const types = keys.map((key) => {
+  const {
+    description,
+    type,
+    properties,
+  } = dataObjects[key];
 
-console.log('OrderValueLimits', allDefinitions['OrderValueLimits']);
+  return {
+    kind: toUpper(type), // there is something wrong here
+    name: key,
+    description,
+    fields: getFields(properties),
+    interfaces: [],
+    enumValues: null,
+    possibleTypes: null,
+    // debugData: dataObjects[key],
+  }
+  
+  return dataObjects[key];
+})
 
-const final = JSON.stringify(allDefinitions, null, 2);
+const introspection = {
+  data: {
+    __schema: {
+      queryType: {
+        name: 'Account',
+      },
+      mutationType: null,
+      subscriptionType: null,
+      types: [
+        ...getScalars(dataObjects),
+        ...types,
+        ...graphQLTypes,
+      ],
+      directives: [],
+    }
+  }
+}
+
+const final = JSON.stringify(introspection, null, 2);
 
 // console.log(JSON.stringify(allDefinitions, null, 2));
 
